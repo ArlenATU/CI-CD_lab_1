4bad2ead6aa0d7a1da4c33672f5b8111c052f6c1
DenysKapustianskyi-S <G00470377@atu.ie>
2026-09-23 14:49:45 +0100

    Changed code in Service to use JPA

--- src/main/java/com/example/lab_1/repository/ProductRepository.java
+++ src/main/java/com/example/lab_1/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.example.lab_1.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
 }
